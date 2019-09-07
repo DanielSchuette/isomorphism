ec2c25a111cf6e6afb2bcfcd358b84e79db865af
DanielSchuette <d.schuette@online.de>
2019-09-07 22:26:15 +0200

    add similar degree distribution checking

--- src/isomorphism.c
+++ src/isomorphism.c
@@ -1,5 +1,6 @@
 /* isomorphism.c: compare two graphs with each other. */
 #include <errno.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -61,6 +62,7 @@ void init_graph(graph *graph)
     graph->vertices = 0;
     graph->num_edges = 0;
     graph->edges = NULL;
+    graph->degrees = NULL;
 }
 
 int read_graph_file(char *name, graph *graph)
@@ -86,8 +88,14 @@ int read_graph_file(char *name, graph *graph)
         if (idx == 0) {
             graph->vertices = e1;
             graph->num_edges = e2;
+
+            /* allocate memory to store edges */
             graph->edges = (edge *)malloc(sizeof(edge) * e2);
             if (!graph->edges) return -1;
+
+            /* allocate memory to store degree of every vertex */
+            graph->degrees = (uint64_t *)malloc(sizeof(uint64_t) * e1);
+            if (!graph->degrees) return -1;
         } else {
             /* ensure there aren't too many edges read */
             if (idx > graph->num_edges)
@@ -103,12 +111,30 @@ int read_graph_file(char *name, graph *graph)
     if ((idx-1) != graph->num_edges)
         fail("too few edges specified", 1);
 
+    calc_degrees(graph);
+
     return 0;
+}
+
+void calc_degrees(graph *graph)
+{
+    uint64_t edge, idx1, idx2;
+
+    for (edge = 0; edge < graph->num_edges; edge++) {
+        idx1 = graph->edges[edge].vert1;
+        idx2 = graph->edges[edge].vert2;
+        if (idx1 == idx2)
+            fail("pseudographs with loops aren't allowed", 1);
+
+        graph->degrees[idx1]++;
+        graph->degrees[idx2]++;
+    }
 }
 
 void free_graph(graph *graph)
 {
     free(graph->edges);
+    free(graph->degrees);
 }
 
 void dump_graph(graph *graph)
@@ -122,12 +148,68 @@ void dump_graph(graph *graph)
 
 graph_stats is_isomorphic(graph *g1, graph *g2)
 {
+    uint64_t *dist1, *dist2;
+
+    /* check for equal vertices and edges */
     if (g1->vertices != g2->vertices)
         return VERTICES_UEQ;
     if (g1->num_edges != g2->num_edges)
         return EDGES_UEQ;
 
-    /* FIXME: cover degrees, graph pieces, and detailed analysis */
+    /* compare degree distributions */
+    dist1 = (uint64_t *)malloc(sizeof(uint64_t) * g1->vertices);
+    dist2 = (uint64_t *)malloc(sizeof(uint64_t) * g2->vertices);
+    if (!(dist1 && dist2))
+        fail(strerror(errno), 1);
+
+    get_dist(dist1, g1);
+    get_dist(dist2, g2);
+    if (!comp_dist(dist1, dist2, g1->vertices, g1->num_edges))
+        return DEGREES_UEQ;
+
+    /* TODO: check graph pieces */
+    /* TODO: detailed analysis */
 
     return ISOMORPHIC;
+}
+
+void get_dist(uint64_t *d, graph *g)
+{
+    uint64_t cnt, vert, edge;
+
+    for (vert = 0; vert < g->vertices; vert++) {
+        cnt = 0;
+        for (edge = 0; edge < g->num_edges; edge++) {
+            if (g->edges[edge].vert1 == vert || g->edges[edge].vert2 == vert)
+                cnt++;
+        }
+        d[vert] = cnt;
+    }
+}
+
+bool comp_dist(uint64_t *d1, uint64_t *d2, uint64_t len, uint64_t edges)
+{
+    bool res = true;
+    uint64_t vert, degr, cnt1, cnt2;
+
+    /*
+     * For every possible degree (max is number of edges in graphs), test if
+     * both graphs have the same number of vertices with that particular
+     * degree.
+     */
+    for (degr = 0; degr < edges; degr++) {
+        cnt1 = cnt2 = 0;
+        for (vert = 0; vert < len; vert++) {
+            if (d1[vert] == degr) cnt1++;
+            if (d2[vert] == degr) cnt2++;
+        }
+        if (cnt1 != cnt2) {
+            res = false;
+            break;
+        }
+    }
+
+    free(d1);
+    free(d2);
+    return res;
 }
--- src/isomorphism.h
+++ src/isomorphism.h
@@ -1,12 +1,14 @@
 #ifndef _ISOMORPHISM_H_
 #define _ISOMORPHISM_H_
 
+#include <stdbool.h>
 #include <stdint.h>
 
-#define fail(msg, code) {                                           \
-    fprintf(stderr, "%s (%d): %s.\n", progname, __LINE__, msg);     \
-    exit(code);                                                     \
-}
+#define fail(msg, code)                                             \
+    {                                                               \
+        fprintf(stderr, "%s (%d): %s.\n", progname, __LINE__, msg); \
+        exit(code);                                                 \
+    }
 
 typedef struct {
     uint64_t vert1;
@@ -16,22 +18,27 @@ typedef struct {
 typedef struct {
     uint64_t vertices;
     uint64_t num_edges;
+    uint64_t *degrees;
     edge *edges;
 } graph;
 
 typedef enum {
-    VERTICES_UEQ,   /* unequal number of vertices */
-    EDGES_UEQ,      /* unequal number of edges */
-    DEGREES_UEQ,    /* unequal distribution of vertex degree values */
-    PIECES_UEQ,     /* unequal number of pieces */
-    UNEQUAL,        /* unequal after detailed analysis */
-    ISOMORPHIC,     /* isomorphic graphs */
+    VERTICES_UEQ, /* unequal number of vertices */
+    EDGES_UEQ,    /* unequal number of edges */
+    DEGREES_UEQ,  /* unequal distribution of vertex degree values */
+    PIECES_UEQ,   /* unequal number of pieces */
+    UNEQUAL,      /* unequal after detailed analysis */
+    ISOMORPHIC,   /* isomorphic graphs */
 } graph_stats;
 
 void init_graph(graph *);
 int read_graph_file(char *, graph *);
+void calc_degrees(graph *);
 void free_graph(graph *);
 void dump_graph(graph *);
 graph_stats is_isomorphic(graph *, graph *);
+
+void get_dist(uint64_t *d, graph *g);
+bool comp_dist(uint64_t *, uint64_t *, uint64_t, uint64_t);
 
 #endif /* _ISOMORPHISM_H_ */
